d7d1ac18bd4d3be05131bea32ece1e0dab9050f4
oLfat0 <otaviogabrielso135@outlook.com>
2026-05-28 21:32:49 -0400

    wip: nomes de variáveis modificadas, estrutura de Strings corrigida(char *x[]), criação da estrutura da função 'insere'

--- main_b-tree.c
+++ main_b-tree.c
@@ -1,3 +1,15 @@
+/*
+
+Seja d um número natural. Uma árvore B de ordem d é uma árvore ordenada que é vazia
+ou que satisfaz as seguintes condições:
+
+(i) a raiz é uma folha ou possui no mínimo dois filhos;
+(ii) cada nó interno (exceto a raiz) possui no mínimo d + 1 filhos;
+(iii) cada nó possui no máximo 2d + 1 filhos;
+(iv) todas as folhas estão no mesmo nível.
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,17 +17,18 @@
 
 typedef struct no
 {
-    int n;
-    char chaves[2*D][15];
-    struct no* filhos[(2*D)+1];
-}no;
+    int n;                      //Quantidade de chaves no Nó
+    char *chaves[D];            //Lista de String -> Cada chave é uma String
+    struct no* filhos[D+1];
 
-no* novo_no();
+}No;
+
+No* novo_no(int n);
 void menu();
-void busca(no* raiz, char c[]);
-void insere(no* raiz, char nome[]);
+void busca(No* pt, char *x[]);
+void insere(No* raiz, char nome[]);
 
-no* raiz = NULL;
+No* raiz = NULL;
 
 int main()
 {
@@ -25,8 +38,23 @@ int main()
     }
 }
 
-void menu()
-{
+No* novo_no(int n){
+    No* novo = malloc(sizeof(No));
+    novo->n = n;
+
+    for(int i=0; i<n; i++){
+        novo->chaves[i] = '\0';
+    }
+
+    for(int i=0; i<n+1; i++){
+        novo->filhos[i] = NULL;
+    }
+
+
+    return novo;
+}
+
+void menu(){
     int i= 0;
     printf("// ----- // ----- // ARVORE B // ----- // ----- //\n");
     printf("[1] - Buscar\n");
@@ -50,48 +78,37 @@ void menu()
     }
 }
 
-no* novo_no()
-{
-    no* novo = malloc(sizeof(no));
-    novo->n = 0;
-    for(int i = 0; i < (2*D)+1; i++)
-    {
-        novo->filhos[i] = NULL;
-    }
-    for(int i = 0; i <2*D; i++)
-    {
-        novo->chaves[i][0] = '\0';
-    }
-    return novo;
-}
-
-void busca(no* raiz, char c[])
-{
-    if(raiz == NULL)
-    {
+void busca(No* pt, char *x){
+    if(pt == NULL){
         printf("Elemento nao encontrado\n");
         return;
     }
-    int i = 0;
-    while(i < raiz->n && strcmp(c,raiz->chaves[i])>0){i++;}
 
-    if(strcmp(c,raiz->chaves[i])==0)
-    {
+    int i = 0;
+
+    while(i < pt->n){
+        if(strcmp(x, pt->chaves[i])>0){
+            i++;
+        }
+    }
+
+    if(strcmp(x, pt->chaves[i])==0){
         printf("Elemento encontrado!\n");
         return;
     }
-    busca(raiz->filhos[i],c);
-}
-void insere(no* raiz, char nome[])
-{
-    if(raiz == NULL)
-    {
-        raiz = novo_no();
-        for(int i = 0; i <= strlen(nome); i ++)
-            raiz->chaves[0][i] = nome[i];
-        raiz->n++;
-        return;
-    }
-    
 
+    busca(pt->filhos[i], x);
+}
+
+void insere(No* raiz, char *nome){
+    if(raiz==NULL){                 //Caso a árvore seja vazia
+        raiz->chaves[0] = nome;
+    }
+
+    int i=0;
+    while(raiz->chaves[i]!='\0'){
+        if(strcmp(raiz->chaves[i], nome) == 0){
+
+        }
+    }
 }
